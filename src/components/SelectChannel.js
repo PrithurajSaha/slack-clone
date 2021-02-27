8e2597874c74bf382edf563f8bfa2256f105258b
PrithurajSaha <prithuraj2009@gmail.com>
2021-02-27 16:10:45 +0530

    donew

--- src/components/SelectChannel.js
+++ src/components/SelectChannel.js
@@ -1,7 +1,16 @@
+import styled from "styled-components";
 import React from "react";
 
 function SelectChannel() {
-  return <div>Select a channel or create one</div>;
+  return <Container>Select a channel or create one </Container>;
 }
 
 export default SelectChannel;
+
+const Container = styled.div`
+  color: #eeeeee;
+  font-size: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
